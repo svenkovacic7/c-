993756596e78260f228afb121b34d2ed687575de
svenkovacic7 <sven.kovacic.7@gmail.com>
2017-05-16 18:45:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,81 +1,59 @@
-/* 
-Idemo delat!
-*/
-#include <cstdlib>
+#include <SFML/Graphics.hpp>
 #include <iostream>
-#include <string>
-#include <cmath>
-#include <winbgim.h>
-using namespace std;
+#include <Windows.h>
+#include <ctime>
 
-int main() {
-    char move;
-    int a,b,c,d;
-    int lina,linb,linc,lind;
-    a = 10;
-    b = 20;
-    c = 30;
-    d = 40;
-    lina = 100;
-    linb = 302;
-    linc = 560;
-    lind = 302;
-	// ovdje ide moj slatki program ;)
-	int gdriver = 9; // Ovdje odreðujemo velièinu grafièkog prozora i broj boja.
-	int gmode = 2;
-	initgraph(&gdriver, &gmode, "");  // Otvaranje grafièkog prozora.
-	setbkcolor(WHITE);		// Odreðivanje boje pozadine.
-	setcolor(BLACK);		// Odreðivanje boje crte.
-	cleardevice(); // Briše stari sadržaj zaslona i stare postavke boja. Pokušajmo pokrenuti program bez ove naredbe.
-	rectangle(a,b,c,d);		// Naredba za crtanje kruga.
-	line(lina,linb,linc,lind);
-    			// kao i system(“PAUSE”) u “normalnim” programima.
-	while (true){
-        switch(getch()){
-            case 80:
-                if(d < linb  || c < lina || a > linc ||b > linb-5){
-                cleardevice();
-                b+= 5;
-                d+=5;
-                rectangle(a,b,c,d);
-                line(lina,linb,linc,lind);
-            }
-                else{
-                    cleardevice();
-                    line(lina,linb,linc,lind);
-                    rectangle(a,b,c,d);
-                }
-                break;
-            case 72:
-                if( b > linb  || c < lina || a > linc ||d < linb+5){
-                cleardevice();
-                b-=5;
-                d-=5;
-                rectangle(a,b,c,d);
-                line(lina,linb,linc,lind);
-            }
-                else{
-                    cleardevice();
-                    line(lina,linb,linc,lind);
-                    rectangle(a,b,c,d);
-            }
-                break;
-            case 77:
-                cleardevice();
-                a+=5;
-                c+=5;
-                rectangle(a,b,c,d);
-                line(lina,linb,linc,lind);
-                break;
-            case 75:
-                cleardevice();
-                a-=5;
-                c-=5;
-                rectangle(a,b,c,d);
-                line(lina,linb,linc,lind);
-                break;           
-        }
-    }
-	closegraph();		// Zatvaranje grafièkog prozora.
-	return 0;			// Ovo znaju i ptice na grani..
+void moving(sf::RectangleShape &rectangle) {
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	{
+		sf::Vector2f position = rectangle.getPosition();
+		rectangle.setPosition(position.x, position.y + 5);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	{
+		sf::Vector2f position = rectangle.getPosition();
+		rectangle.setPosition(position.x, position.y - 5);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		sf::Vector2f position = rectangle.getPosition();
+		rectangle.setPosition(position.x + 5, position.y);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		sf::Vector2f position = rectangle.getPosition();
+		rectangle.setPosition(position.x - 5, position.y);
+	}
+}
+
+int main()
+{
+	const int posA = 100; //Starting X rectangle
+	const int posB = 100; //Starting Y rectangle
+	sf::RenderWindow window(sf::VideoMode(600, 600), "SFML works!");
+	window.setFramerateLimit(60);
+	sf::RectangleShape rectangle(sf::Vector2f(120, 50));
+	sf::CircleShape circle(25);
+	rectangle.setFillColor(sf::Color::Red);
+	rectangle.setPosition(posA, posB);
+	circle.setPosition(400, 250);
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		if (rectangle.getGlobalBounds().intersects(circle.getGlobalBounds())) {
+			rectangle.setPosition(posA, posB);
+		}
+		moving(rectangle);
+		window.clear();
+		window.draw(rectangle);
+		window.draw(circle);
+		window.display();
+	}
+
+	return 0;
 }
